797e0a3ef37df0189f007360f89081481431f126
heywb457 <heywb457@naver.com>
2019-09-03 11:05:50 +0900

    올립니다

--- dailyLog/src/main/java/com/kh/with/admin/controller/AdminController.java
+++ dailyLog/src/main/java/com/kh/with/admin/controller/AdminController.java
@@ -36,7 +36,8 @@ public class AdminController {
 	@RequestMapping(value="adminChattingComplete.ad")
 	public String adminChattingComplete() {
 		return "admin/adminChattingComplete";
-
+	}
+	
 	//관리자 공지사항 수정 메소드 
 	@RequestMapping(value="adminNoticeRevise.ad")
 	public String adminNoticeRevise() {
