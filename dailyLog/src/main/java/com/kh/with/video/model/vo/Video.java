24dc3ca982b242ed0be3e03aabcc2539971b5285
yjdld <yjdld@DESKTOP-7K3LEMM>
2019-09-20 12:49:28 +0900

    loger,main 매퍼 video,attachment vo 수정

--- dailyLog/src/main/java/com/kh/with/video/model/vo/Video.java
+++ dailyLog/src/main/java/com/kh/with/video/model/vo/Video.java
@@ -17,16 +17,16 @@ public class Video implements java.io.Serializable {
 	private String adInfo; //광고정보
 	private String location; //위치
 	private String info; //추가장소정보
-	private String filepath; // 파일경로
+	private String fileNm; // 파일경로
 	private String openTy; //공개여부
 	private int nNo;
-	private String afilepath;
+	private String afileNm;
 	
 	public Video () {}
 
 	public Video(int vNo, int userNo, String vTitle, int count, String chNm, String tag, String vblYn, Date uploadDt,
-			Date modifyDt, String adultAut, String adYn, String adInfo, String location, String info, String filepath,
-			String openTy, int nNo, String afilepath) {
+			Date modifyDt, String adultAut, String adYn, String adInfo, String location, String info, String fileNm,
+			String openTy, int nNo, String afileNm) {
 		super();
 		this.vNo = vNo;
 		this.userNo = userNo;
@@ -42,10 +42,10 @@ public class Video implements java.io.Serializable {
 		this.adInfo = adInfo;
 		this.location = location;
 		this.info = info;
-		this.filepath = filepath;
+		this.fileNm = fileNm;
 		this.openTy = openTy;
 		this.nNo = nNo;
-		this.afilepath = afilepath;
+		this.afileNm = afileNm;
 	}
 
 	public int getvNo() {
@@ -160,12 +160,12 @@ public class Video implements java.io.Serializable {
 		this.info = info;
 	}
 
-	public String getFilepath() {
-		return filepath;
+	public String getFileNm() {
+		return fileNm;
 	}
 
-	public void setFilepath(String filepath) {
-		this.filepath = filepath;
+	public void setFileNm(String fileNm) {
+		this.fileNm = fileNm;
 	}
 
 	public String getOpenTy() {
@@ -184,12 +184,12 @@ public class Video implements java.io.Serializable {
 		this.nNo = nNo;
 	}
 
-	public String getAfilepath() {
-		return afilepath;
+	public String getAfileNm() {
+		return afileNm;
 	}
 
-	public void setAfilepath(String afilepath) {
-		this.afilepath = afilepath;
+	public void setAfileNm(String afileNm) {
+		this.afileNm = afileNm;
 	}
 
 	@Override
@@ -197,8 +197,8 @@ public class Video implements java.io.Serializable {
 		return "Video [vNo=" + vNo + ", userNo=" + userNo + ", vTitle=" + vTitle + ", count=" + count + ", chNm=" + chNm
 				+ ", tag=" + tag + ", vblYn=" + vblYn + ", uploadDt=" + uploadDt + ", modifyDt=" + modifyDt
 				+ ", adultAut=" + adultAut + ", adYn=" + adYn + ", adInfo=" + adInfo + ", location=" + location
-				+ ", info=" + info + ", filepath=" + filepath + ", openTy=" + openTy + ", nNo=" + nNo + ", afilepath="
-				+ afilepath + "]";
+				+ ", info=" + info + ", fileNm=" + fileNm + ", openTy=" + openTy + ", nNo=" + nNo + ", afileNm="
+				+ afileNm + "]";
 	}
 
 	
