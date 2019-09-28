e2d1f7be13dd0e222f15213790f753a090746cb3
heywb457 <heywb457@naver.com>
2019-09-29 04:43:14 +0900

    비디오메인은 채원이껄로 덮어주세요~

--- dailyLog/src/main/java/com/kh/with/video/model/dao/VideoDao.java
+++ dailyLog/src/main/java/com/kh/with/video/model/dao/VideoDao.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import org.mybatis.spring.SqlSessionTemplate;
 
 import com.kh.with.loger.model.vo.Loger;
+import com.kh.with.loger.model.vo.Support;
+import com.kh.with.main.model.vo.VideoLike;
 import com.kh.with.member.model.vo.Member;
 import com.kh.with.report.model.vo.Report;
 import com.kh.with.video.model.vo.AddInfo;
@@ -55,7 +57,7 @@ public interface VideoDao {
 	
 	List<Loger> selectLogerInfo(SqlSessionTemplate sqlSession, Map<String, Object> map);
 	
-	int selectRegStatus(SqlSessionTemplate sqlSession, int userNo, int chNo);
+	List<Support> selectRegStatus(SqlSessionTemplate sqlSession, Map<String, Integer> map);
 	
 	int insertReg(SqlSessionTemplate sqlSession, Map<String, Integer> map);
 
@@ -106,10 +108,10 @@ public interface VideoDao {
 	int deleteSubAlram(SqlSessionTemplate sqlSession, Map<String, Object> map);
 	String selectThumb(SqlSessionTemplate sqlSession, Map<String, Object> map);
 	String selectProfile(SqlSessionTemplate sqlSession, Map<String, Object> map);
-	int selectLike(SqlSessionTemplate sqlSession, Map<String, Integer> map);
-	int selectHate(SqlSessionTemplate sqlSession, Map<String, Integer> map);
-	int selectBook(SqlSessionTemplate sqlSession, Map<String, Integer> map);
-	int selectRegStatus(SqlSessionTemplate sqlSession, Map<String, Integer> map);
+	List<VideoLike> selectLike(SqlSessionTemplate sqlSession, Map<String, Integer> map);
+	List<VideoLike> selectHate(SqlSessionTemplate sqlSession, Map<String, Integer> map);
+	List<VideoLike> selectBook(SqlSessionTemplate sqlSession, Map<String, Integer> map);
+	
 	List<Reply2> selectReply(SqlSessionTemplate sqlSession, Map<String, Object> map);
 	String selectUserImg(SqlSessionTemplate sqlSession, Map<String, Object> map);
 	int insertReply(SqlSessionTemplate sqlSession, Map<String, Object> map);
@@ -146,6 +148,13 @@ public interface VideoDao {
 
 	List<AddInfo> selectAddInfo(SqlSessionTemplate sqlSession, Map<String, Object> map);
 	List<AddPlace> selectAddPlace(SqlSessionTemplate sqlSession, Map<String, Object> map);
+	List<Attachment> selectFiles(SqlSessionTemplate sqlSession);
+	int vStatus(SqlSessionTemplate sqlSession, Map<String, Integer> map);
+	int channelReport(SqlSessionTemplate sqlSession, Map<String, Object> map);
+	int channelReportAlram(SqlSessionTemplate sqlSession, Map<String, Object> map);
+	List<VideoLike> selectReplyLike(SqlSessionTemplate sqlSession, Map<String, Integer> map);
+	List<VideoLike> selectReplyHate(SqlSessionTemplate sqlSession, Map<String, Integer> map);
+	int selectSubtitle(SqlSessionTemplate sqlSession, Map<String, Object> map);
 
 
 
